6abc2f90c0cb236837942f6cda0cad67478fe385
AB244 <ajbenn04@louisville.edu>
2014-11-23 17:40:21 -0500

    Completed basic broadcast intent tutorial.

---
+++ app/src/main/java/com/cis490/alex/hw6/MyReceiver.java
@@ -0,0 +1,17 @@
+package com.cis490.alex.hw6;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by Alex on 11/23/2014.
+ */
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Intent Detected.", Toast.LENGTH_LONG).show();
+    }
+}
